24d139db37bc92ffe6e90a325fb97bcab836d4f3
hamza-dev-tech <hamzadevtech01@gmail.com>
2024-07-25 22:00:06 +0500

    rollback

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-     
+        <AuthProvider>
           <ThemeContextProvider>
             <ThemeProvider>
               <div className="container">
@@ -29,7 +30,7 @@ export default function RootLayout({ children }) {
               </div>
             </ThemeProvider>
           </ThemeContextProvider>
-   
+        </AuthProvider>
       </body>
     </html>
   );
